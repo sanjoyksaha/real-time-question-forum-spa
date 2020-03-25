9cf9bee879861af93c068e1154f386252678b94e
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-03-25 20:32:13 +0600

    Creating Migration, Model, Controller, Factories

---
+++ database/migrations/2020_03_25_133118_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
