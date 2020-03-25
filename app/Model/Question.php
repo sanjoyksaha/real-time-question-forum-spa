54f30cab97021de5b2cf274ce9b71b650e8d4309
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-03-25 21:02:20 +0600

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,23 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
